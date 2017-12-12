cff9c3405f46965af919a6c9ddc668336ac07428
Nicholas Car <nicholas.car@ga.gov.au>
2017-12-12 20:05:48 +1100

    added Sites XML API endpoints

--- _config/__init__.py
+++ _config/__init__.py
@@ -6,22 +6,20 @@ STATIC_DIR = join(dirname(dirname(abspath(__file__))), 'view', 'static')
 LOGFILE = APP_DIR + '/flask.log'
 DEBUG = True
 
-XML_API_URL_SITESET = 'http://dbforms.ga.gov.au/www_distp/a.igsn_api.get_igsnSampleSet' \
-                        '?pOrder=IGSN&pPageNo={0}&pNoOfLinesPerPage={1}'
-XML_API_URL_SITE = 'http://dbforms.ga.gov.au/www_distp/a.igsn_api.get_igsnSample?pIGSN={0}'
-XML_API_URL_NETWORKSET = 'http://dbforms.ga.gov.au/www_distp/a.igsn_api.get_igsnSampleSet' \
-                        '?pOrder=IGSN&pPageNo={0}&pNoOfLinesPerPage={1}'
-XML_API_URL_NETWORK = 'http://dbforms.ga.gov.au/www_distp/a.igsn_api.get_igsnSample?pIGSN={0}'
+XML_API_URL_SITESET = 'http://dbforms.ga.gov.au/www/a.entities_api.SearchEntities' \
+                        '?pOrder=ENO&pPageNo={0}&pNoOfRecordsPerPage={1}'
+XML_API_URL_SITE = 'http://dbforms.ga.gov.au/www/a.entities_api.entities?pEno={0}'
+# XML_API_URL_NETWORKSET = ''
+# XML_API_URL_NETWORK = ''
 
 XML_API_URL_SITESET_DATE_RANGE = \
-    'http://dbforms.ga.gov.au/www_distp/a.igsn_api.get_igsnSampleSet' \
-    '?pOrder=IGSN&pPageNo={0}&pNoOfLinesPerPage={1}&pModifiedFromDate={2}' \
-    '&pModifiedToDate={3}'
+    'http://dbforms.ga.gov.au/www/a.entities_api.SearchEntities' \
+    '?pOrder=ENO&pPageNo={0}&pNoOfRecordsPerPage={1}&pStartEntryDate={2}' \
+    '&pEndEntryDate={3}'
 
-XML_API_URL_MIN_DATE = 'http://dbforms.ga.gov.au/www_distp/a.igsn_api.get_Earliest_Date_Modified'
-XML_API_URL_TOTAL_COUNT = 'http://dbforms.ga.gov.au/www_distp/a.igsn_api.get_Number_Modified'
-XML_API_URL_TOTAL_COUNT_DATE_RANGE = 'http://dbforms.ga.gov.au/www_distp/a.igsn_api.get_Number_Modified'\
-                                     '?pModifiedFromDate={0}&pModifiedToDate={1}'
+XML_API_URL_SITES_TOTAL_COUNT = 'http://dbforms.ga.gov.au/www/a.entities_api.get_total_number_records'
+XML_API_URL_SITES_TOTAL_COUNT_DATE_RANGE = 'http://dbforms.ga.gov.au/www/a.entities_api.get_Number_Modified?' \
+                                           'pModifiedFromDate={0}&pModifiedToDate={1}'
 
 ADMIN_EMAIL = 'dataman@ga.gov.au'
 
@@ -29,8 +27,6 @@ URI_NETWORK_CLASS = 'http://pid.geoscience.gov.au/def/ont/ga/pdm#SiteNetwork'
 URI_NETWORK_INSTANCE_BASE = 'http://pid.geoscience.gov.au/network/'
 URI_SITE_CLASS = 'http://pid.geoscience.gov.au/def/ont/ga/pdm#Sample'
 URI_SITE_INSTANCE_BASE = 'http://pid.geoscience.gov.au/site/'
-
-
 
 GOOGLE_MAPS_API_KEY_EMBED = 'AIzaSyDhuFCoJynhhQT7rcgKYzk3i7K77IEwjO4'
 GOOGLE_MAPS_API_KEY = 'AIzaSyCUDcjVRsIHVHpv53r7ZnaX5xzqJbyGk58'
